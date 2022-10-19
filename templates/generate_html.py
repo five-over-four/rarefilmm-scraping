857e2f4d38da2a234156e45c1e0b9614cd2ff312
five-over-four <73562879+five-over-four@users.noreply.github.com>
2022-10-19 23:15:41 +0300

    created generate_html.py
    This package is necessary for generating arbitrary numbers of divs. will be used later to generate the movie recommendation blocks in the results page.

---
+++ templates/generate_html.py
@@ -0,0 +1,33 @@
+# This file generates the html blocks for displaying multiple movies in a row fashion.
+
+# set this from within ../main.py to the list of rf_data.csv rows.
+# that are being recommended.
+MOVIES = None
+
+def generate_html_block(movie):
+    """
+    Generates the boilerplate movie block HTML to be embedded.
+    takes a dictionary of the form
+
+    movie = {"year": film.year, "title": film.title, "poster": film.poster, "overview": film.overview}
+
+    * Doesn't actually have to be a film, but that's the primary purpose.
+    """
+    movie_format = f"""
+    <div class='movie'>
+            
+            <div class='poster-details-parent'>
+
+                <div class='poster'><img src ='{movie["poster"]}'></div>
+
+                <div class='details'>
+
+                    <h1>{movie["title"]} {movie["year"]}</h1>
+
+                {movie["overview"]}
+                </div>
+
+            </div>
+
+        </div>"""
+    return movie_format
